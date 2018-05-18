8d817292f34706f59128ca057caaf50539ef8b3a
ice <haozhiyx@outlook.com>
2018-05-18 16:22:29 +0800

    review

--- take_off.py
+++ take_off.py
@@ -13,6 +13,10 @@ class TakeOff():
             init_angle_velocities: initial radians/second for each of the three Euler angles
             runtime: time limit for each episode
             target_pos: target/goal (x,y,z) position for the agent
+            起点（0，0，0）
+            终点 高度大于20
+            路径 接近垂直上升
+
         """
         # Simulation
         self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime)
@@ -22,13 +26,30 @@ class TakeOff():
         self.action_low = 0
         self.action_high = 900
         self.action_size = 4
+        self.last_post = self.sim.pose
 
         # Goal
         self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
-        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        #1、计算当前位置与目标位置差值的绝对值
+        temp = abs(self.sim.pose[:3] - self.target_pos)
+        #2、(self.sim.pose[2] - self.target_pos[2]) 当前位置与目标位置高度的差值作为奖励的第一部分，
+        # 当前位置低于目标位置奖励为负值，当前位置高于目标位置奖励为正值；
+        # 奖励的第二部分是当前位置与目标位置xy轴上的偏差，偏差越大奖励越小，防止上升时xy轴上有较大偏移
+        reward = (self.sim.pose[2] - self.target_pos[2]) - 0.5 * temp[:2].sum()
+        # 3、奖励的第三部分是当起飞高度大于目标位置高度时奖励增加20，小于目标位置高度时奖励减少30；可以让飞行器更快的达到起飞高度
+        if self.sim.pose[2] >= self.target_pos[2]:  # agent has crossed the target height
+            reward += 20.0  # bonus reward
+        else:
+            reward -= 30
+        #4、奖励的第四部分是当前位置高度与上次位置高度的差值，若当前位置高度低于上次位置高度说明飞行器在下降奖励减少，
+        # 当前位置高度等于上次位置高度奖励不变，当前位置高度高于上次位置高度说明飞行器在上升，奖励增加
+        temp = self.sim.pose - self.last_post
+        reward += temp[2]
+
+
         return reward
 
     def step(self, rotor_speeds):
